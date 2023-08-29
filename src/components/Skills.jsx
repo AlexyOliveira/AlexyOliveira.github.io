42dca9529e798d7b67198da0db49292a7444f5d7
AlexyOliveira <cupofteaoficial@gmail.com>
2023-08-29 12:01:48 -0300

    add new skill icons

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -23,18 +23,211 @@ export default function Skills() {
         Habili<span>dades</span>
       </h2>
       <div className="skills-content">
-        <SiJavascript className="skill-icon"></SiJavascript>
-        <SiHtml5 className="skill-icon" />
-        <SiCss3 className="skill-icon" />
-        <SiReact className="skill-icon" />
-        <SiNodedotjs className="skill-icon" />
-        <SiMysql className="skill-icon" />
-        <SiExpress className="skill-icon" />
-        <SiGit className="skill-icon" />
-        <SiSequelize className="skill-icon" />
-        <SiTypescript className="skill-icon" />
-        <SiBootstrap className="skill-icon" />
-        <SiPython className="skill-icon" />
+        <div className="skills-container">
+          <div>
+            <img
+              width="70"
+              src="https://user-images.githubusercontent.com/25181517/192108372-f71d70ac-7ae6-4c0d-8395-51d8870c2ef0.png"
+              alt="Git"
+              title="Git"
+            />
+          </div>
+          <div>
+            <img
+              width="70"
+              src="https://user-images.githubusercontent.com/25181517/192108374-8da61ba1-99ec-41d7-80b8-fb2f7c0a4948.png"
+              alt="GitHub"
+              title="GitHub"
+            />
+          </div>
+          <div>
+            <img
+              width="70"
+              src="https://user-images.githubusercontent.com/25181517/192108891-d86b6220-e232-423a-bf5f-90903e6887c3.png"
+              alt="Visual Studio Code"
+              title="Visual Studio Code"
+            />
+          </div>
+          <div>
+            <img
+              width="70"
+              src="https://user-images.githubusercontent.com/25181517/192158954-f88b5814-d510-4564-b285-dff7d6400dad.png"
+              alt="HTML"
+              title="HTML"
+            />
+          </div>
+          <div>
+            <img
+              width="70"
+              src="https://user-images.githubusercontent.com/25181517/183898674-75a4a1b1-f960-4ea9-abcb-637170a00a75.png"
+              alt="CSS"
+              title="CSS"
+            />
+          </div>
+          <div>
+            <img
+              width="70"
+              src="https://user-images.githubusercontent.com/25181517/183898054-b3d693d4-dafb-4808-a509-bab54cf5de34.png"
+              alt="Bootstrap"
+              title="Bootstrap"
+            />
+          </div>
+          <div>
+            <img
+              width="70"
+              src="https://user-images.githubusercontent.com/25181517/189715289-df3ee512-6eca-463f-a0f4-c10d94a06b2f.png"
+              alt="Figma"
+              title="Figma"
+            />
+          </div>
+          <div>
+            <img
+              width="70"
+              src="https://user-images.githubusercontent.com/25181517/117447155-6a868a00-af3d-11eb-9cfe-245df15c9f3f.png"
+              alt="JavaScript"
+              title="JavaScript"
+            />
+          </div>
+
+          <div>
+            <img
+              width="70"
+              src="https://user-images.githubusercontent.com/25181517/183897015-94a058a6-b86e-4e42-a37f-bf92061753e5.png"
+              alt="React"
+              title="React"
+            />
+          </div>
+          <div>
+            <img
+              width="70"
+              src="https://user-images.githubusercontent.com/25181517/183890598-19a0ac2d-e88a-4005-a8df-1ee36782fde1.png"
+              alt="TypeScript"
+              title="TypeScript"
+            />
+          </div>
+          <div>
+            <img
+              width="70"
+              src="https://user-images.githubusercontent.com/25181517/121401671-49102800-c959-11eb-9f6f-74d49a5e1774.png"
+              alt="npm"
+              title="npm"
+            />
+          </div>
+          <div>
+            <img
+              width="70"
+              src="https://user-images.githubusercontent.com/25181517/183568594-85e280a7-0d7e-4d1a-9028-c8c2209e073c.png"
+              alt="Node.js"
+              title="Node.js"
+            />
+          </div>
+          <div>
+            <img
+              width="70"
+              src="https://user-images.githubusercontent.com/25181517/183859966-a3462d8d-1bc7-4880-b353-e2cbed900ed6.png"
+              alt="Express"
+              title="Express"
+            />
+          </div>
+          <div>
+            <img
+              width="70"
+              src="https://user-images.githubusercontent.com/25181517/187896150-cc1dcb12-d490-445c-8e4d-1275cd2388d6.png"
+              alt="Redux"
+              title="Redux"
+            />
+          </div>
+          <div>
+            <img
+              width="70"
+              src="https://user-images.githubusercontent.com/25181517/187955005-f4ca6f1a-e727-497b-b81b-93fb9726268e.png"
+              alt="Jest"
+              title="Jest"
+            />
+          </div>
+          <div>
+            <img
+              width="70"
+              src="https://user-images.githubusercontent.com/25181517/183423507-c056a6f9-1ba8-4312-a350-19bcbc5a8697.png"
+              alt="Python"
+              title="Python"
+            />
+          </div>
+
+          <div>
+            <img
+              width="70"
+              src="https://user-images.githubusercontent.com/25181517/183896128-ec99105a-ec1a-4d85-b08b-1aa1620b2046.png"
+              alt="MySQL"
+              title="MySQL"
+            />
+          </div>
+          <div>
+            <img
+              width="70"
+              src="https://user-images.githubusercontent.com/25181517/182884177-d48a8579-2cd0-447a-b9a6-ffc7cb02560e.png"
+              alt="mongoDB"
+              title="mongoDB"
+            />
+          </div>
+          <div>
+            <img
+              width="70"
+              src="https://github.com/marwin1991/profile-technology-icons/assets/136815194/82df4543-236b-4e45-9604-5434e3faab17"
+              alt="SQLite"
+              title="SQLite"
+            />
+          </div>
+          <div>
+            <img
+              width="70"
+              src="https://user-images.githubusercontent.com/25181517/117207330-263ba280-adf4-11eb-9b97-0ac5b40bc3be.png"
+              alt="Docker"
+              title="Docker"
+            />
+          </div>
+          <div>
+            <img
+              width="70"
+              src="https://user-images.githubusercontent.com/68279555/200387386-276c709f-380b-46cc-81fd-f292985927a8.png"
+              alt="Cypress"
+              title="Cypress"
+            />
+          </div>
+          <div>
+            <img
+              width="70"
+              src="https://user-images.githubusercontent.com/25181517/201476472-d2f5f644-cfc9-43e5-96d3-c8f40f18b5cb.png"
+              alt="Chai"
+              title="Chai"
+            />
+          </div>
+          <div>
+            <img
+              width="70"
+              src="https://user-images.githubusercontent.com/25181517/201476630-f47cfff6-fdee-4ee1-9092-1793b71b1ca3.png"
+              alt="Mocha"
+              title="Mocha"
+            />
+          </div>
+          <div>
+            <img
+              width="70"
+              src="https://user-images.githubusercontent.com/25181517/186884150-05e9ff6d-340e-4802-9533-2c3f02363ee3.png"
+              alt="Windows"
+              title="Windows"
+            />
+          </div>
+
+          <div>
+            <img
+              width="70"
+              src="https://user-images.githubusercontent.com/25181517/186884153-99edc188-e4aa-4c84-91b0-e2df260ebc33.png"
+              alt="Ubuntu"
+              title="Ubuntu"
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
