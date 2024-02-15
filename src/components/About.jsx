f0cd8209126fcf0f318c16e78596213f5861c962
Alexandre Oliveira <110850217+AlexyOliveira@users.noreply.github.com>
2024-02-14 21:32:06 -0300

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,63 +20,18 @@ export default function About() {
           <span className="change">Nome:</span> Alexandre Oliveira <br />
           <span className="change">Local de nascimento:</span> Içara, Santa
           Catarina, Brasil <br />{" "}
-          <span className="change">Localização atual:</span>Rio Grande do Sul, Brasil <br />
-          <br />
-          <br />
-          
-"Sou um apaixonado por tecnologia e um entusiasta incansável do desenvolvimento de aplicações inovadoras. Com mais de 2 anos de experiência em projetos desafiadores, sendo os últimos 10 meses dedicados a iniciativas independentes, e mais de 1 ano e 2 meses de formação intensiva na renomada escola Trybe, onde me tornei um desenvolvedor web full stack.
-
-Na <a target="_blank" href="https://www.betrybe.com/">
+          <span className="change">Localização atual:</span> Rio Grande do Sul, Brasil <br />
+          <br />  
+"Sou um apaixonado por tecnologia e um entusiasta incansável do desenvolvimento de aplicações inovadoras. Com mais de 2 anos de experiência em projetos desafiadores, sendo os últimos 10 meses dedicados a iniciativas independentes, e mais de 1 ano e 2 meses de formação intensiva na renomada escola <a target="_blank" href="https://www.betrybe.com/">
             <strong>Trybe</strong>
-          </a>{" "}, mergulhei em projetos estimulantes, tanto individuais quanto em equipe, trabalhando com tecnologias líderes de mercado. Durante esse período, adquiri conhecimento prático em metodologias ágeis, além de aprimorar minhas habilidades sociais, tornando-me não apenas um especialista técnico, mas também um colaborador eficaz em ambientes de equipe dinâmicos 🚀🚀.
+          </a>{" "}, onde me tornei um desenvolvedor web full stack.
+
+Na Trybe, mergulhei em projetos estimulantes, tanto individuais quanto em equipe, trabalhando com tecnologias líderes de mercado. Durante esse período, adquiri conhecimento prático em metodologias ágeis, além de aprimorar minhas habilidades sociais, tornando-me não apenas um especialista técnico, mas também um colaborador eficaz em ambientes de equipe dinâmicos 🚀🚀.
 
 Minhas habilidades abrangem o desenvolvimento full stack, com expertise em tecnologias essenciais, como React.js, React Native, JavaScript, Redux, HTML, CSS, Jest/RTL, TypeScript, MySQL, Bootstrap, Node.js e Python. Atualmente, estou imerso no universo do desenvolvimento de aplicativos utilizando React Native, explorando os limites da criação de experiências mobile envolventes e eficientes.
 
 Estou sempre em busca de desafios estimulantes e oportunidades para aplicar minha paixão pelo desenvolvimento de software, contribuindo para projetos inovadores e impactantes. Se você procura um profissional dedicado, criativo e comprometido com resultados excepcionais, estou pronto para fazer a diferença no seu time!"
           <br />
-          <br />
-          "Sou um entusiasta de tecnologia e tenho paixão pelo desenvolvimento
-          de aplicações. Já são 2 anos de projetos e estudos, 10 meses por conta
-          própria e 1 ano e 2 meses de estudos intensos e bem produtivos na escola{" "}
-          <a target="_blank" href="https://www.betrybe.com/">
-            <strong>Trybe</strong>
-          </a>{" "}
-          , onde me formei como desenvolvedor web full stack. Lá, tive a
-          oportunidade de trabalhar em projetos individuais e em grupo,
-          desenvolvendo tecnologias populares do mercado e aprendendo
-          metodologias ágeis, além de aprimorar minhas habilidades sociais 🚀🚀.
-          Conhecimentos: Sou um desenvolvedor Full Stack com experiência em |
-          React.js | React-native | Java Script | Redux | Html | Css | jest/RTL
-          | TypeScripty | MySQL | Bootstrap | Node JS | Python. Atualmente
-          desenvolvendo apps com React-Native.
-          {/* "Olá, meu nome é <span className="change">Alexandre Oliveira</span> e
-          sou <span className="change">brasileiro</span>. Nasci no município de
-          Içara, em Santa Catarina, mas aos 5 anos de idade mudei-me para
-          <span className="change"> Tramandaí</span>, uma bela cidade litorânea
-          do Rio Grande do Sul. Desde cedo, desenvolvi um interesse apaixonado
-          pelo mundo da tecnologia. Em 2021, decidi embarcar em uma jornada de
-          aprendizado autodidata e dediquei seis meses ao estudo intensivo de
-          HTML, CSS, Bootstrap e JavaScript. Essa experiência me proporcionou
-          uma sólida base nessas tecnologias essenciais para o desenvolvimento
-          web. Empolgado com meu progresso, decidi levar minha paixão pela
-          tecnologia ao próximo nível e comecei o curso de Desenvolvimento Web
-          na{" "}
-          <a target="_blank" href="https://www.betrybe.com/">
-            <strong>Trybe.</strong>
-          </a>{" "}
-          em 05/08/2022. Estou animado por fazer parte dessa renomada escola de
-          tecnologia, que genuinamente se preocupa com o sucesso profissional de
-          seus estudantes. Na Trybe, tenho a oportunidade de trabalhar com as
-          tecnologias mais atualizadas do mercado e participar de projetos
-          desafiadores, tanto individualmente como em equipe. Além do
-          conhecimento técnico, também adquiro habilidades em metodologias ágeis
-          e desenvolvo as competências interpessoais tão valorizadas no ambiente
-          profissional. Estou ansioso para alavancar minhas habilidades e
-          conhecimentos na área de desenvolvimento web, buscando oportunidades
-          para aplicar soluções inovadoras e criar experiências online
-          impactantes. Estou empenhado em construir uma carreira de sucesso
-          nessa indústria em constante evolução. Vamos juntos nessa jornada de
-          aprendizado e crescimento!" 🚀🚀 */}
         </p>
       </div>
     </section>
