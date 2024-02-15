3eef19f12605e998e804c43e92b1bf117fd88810
Alexandre Oliveira <110850217+AlexyOliveira@users.noreply.github.com>
2024-02-14 21:25:55 -0300

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,8 +20,18 @@ export default function About() {
           <span className="change">Nome:</span> Alexandre Oliveira <br />
           <span className="change">Local de nascimento:</span> Içara, Santa
           Catarina, Brasil <br />{" "}
-          <span className="change">Localização atual:</span> Litoral Norte do
-          Rio Grande do Sul, Brasil <br />
+          <span className="change">Localização atual:</span>Rio Grande do Sul, Brasil <br />
+          <br />
+          <br />
+          
+"Sou um apaixonado por tecnologia e um entusiasta incansável do desenvolvimento de aplicações inovadoras. Com mais de 2 anos de experiência em projetos desafiadores, sendo os últimos 10 meses dedicados a iniciativas independentes, e mais de 1 ano e 2 meses de formação intensiva na renomada escola Trybe, onde me tornei um desenvolvedor web full stack.
+
+Na Trybe, mergulhei em projetos estimulantes, tanto individuais quanto em equipe, trabalhando com tecnologias líderes de mercado. Durante esse período, adquiri conhecimento prático em metodologias ágeis, além de aprimorar minhas habilidades sociais, tornando-me não apenas um especialista técnico, mas também um colaborador eficaz em ambientes de equipe dinâmicos 🚀🚀.
+
+Minhas habilidades abrangem o desenvolvimento full stack, com expertise em tecnologias essenciais, como React.js, React Native, JavaScript, Redux, HTML, CSS, Jest/RTL, TypeScript, MySQL, Bootstrap, Node.js e Python. Atualmente, estou imerso no universo do desenvolvimento de aplicativos utilizando React Native, explorando os limites da criação de experiências mobile envolventes e eficientes.
+
+Estou sempre em busca de desafios estimulantes e oportunidades para aplicar minha paixão pelo desenvolvimento de software, contribuindo para projetos inovadores e impactantes. Se você procura um profissional dedicado, criativo e comprometido com resultados excepcionais, estou pronto para fazer a diferença no seu time!"
+          <br />
           <br />
           "Sou um entusiasta de tecnologia e tenho paixão pelo desenvolvimento
           de aplicações. Já são 2 anos de projetos e estudos, 10 meses por conta
